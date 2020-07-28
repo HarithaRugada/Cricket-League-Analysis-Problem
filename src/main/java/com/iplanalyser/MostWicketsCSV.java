1c7474b19d491be3be6f8416a474c2c3be12335b
HarithaRugada <haritharugada@gmail.com>
2020-07-28 16:30:26 +0530

    ADDED : Code and test case for top bowling average

---
+++ src/main/java/com/iplanalyser/MostWicketsCSV.java
@@ -0,0 +1,17 @@
+package com.iplanalyser;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class MostWicketsCSV {
+    @CsvBindByName(column = "PLAYER",required = true)
+    public String bowlerName;
+
+    @CsvBindByName(column = "Avg",required = true)
+    public double bowlingAverage;
+
+    @CsvBindByName(column = "4w",required = true)
+    public int fourWickets;
+
+    @CsvBindByName(column = "5w",required = true)
+    public int fiveWickets;
+}
--- src/test/java/com/iplanalyser/CricketAnalyserTest.java
+++ src/test/java/com/iplanalyser/CricketAnalyserTest.java
@@ -6,59 +6,75 @@ import org.junit.Test;
 
 public class CricketAnalyserTest {
     private static final String IPL_2019_MOST_RUNS_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String IPL_2019_MOST_WICKETS_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
 
+    //TC-1
     @Test
     public void givenIPLMostRunsCSVFile_ReturnsTopBattingAverage_InIPL2019() {
         CricketAnalyser cricketAnalyser = new CricketAnalyser();
-        cricketAnalyser.getCricketDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
+        cricketAnalyser.getBattingDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
         String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.AVERAGE);
-        MostRunsCSV[] mostRunCsv = new Gson().fromJson(sortedCricketData, MostRunsCSV[].class);
-        Assert.assertEquals(83.2,mostRunCsv[0].battingAverage,0.0 );
-    }
-
-    @Test
-    public void givenIPLMostRunsCSVFile_ReturnsTopStrikingRate_InIPL2019() {
-        CricketAnalyser cricketAnalyser = new CricketAnalyser();
-        cricketAnalyser.getCricketDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
-        String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.STRIKERATE);
-        MostRunsCSV[] mostRunCsv = new Gson().fromJson(sortedCricketData, MostRunsCSV[].class);
-        Assert.assertEquals(333.33,mostRunCsv[0].strikeRate,0.0 );
-    }
-
-    @Test
-    public void givenIPLMostRunsCSVFile_ReturnsCricketer_WhoHitMaximum4sAnd6s_InIPL2019() {
-        CricketAnalyser cricketAnalyser = new CricketAnalyser();
-        cricketAnalyser.getCricketDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
-        String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.BESTSTRIKE);
-        MostRunsCSV[] mostRunCsv = new Gson().fromJson(sortedCricketData, MostRunsCSV[].class);
-        Assert.assertEquals(31,mostRunCsv[0].fours);
-        Assert.assertEquals(52,mostRunCsv[0].sixes);
-    }
-
-    @Test
-    public void givenIPLMostRunsCSVFile_ReturnsCricketer_WhoHasMaximumStrikeRateWithMaximum4sAnd6s() {
-        CricketAnalyser cricketAnalyser = new CricketAnalyser();
-        cricketAnalyser.getCricketDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
-        String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.BESTSTRIKE);
-        MostRunsCSV[] mostRunCsv = new Gson().fromJson(sortedCricketData, MostRunsCSV[].class);
-        Assert.assertEquals(204.81,mostRunCsv[0].strikeRate, 0.0);
-    }
-
-    @Test
-    public void givenIPLMostRunsCSVFile_When_MaximumAverage_Returns_BestStrikeRate() {
-        CricketAnalyser cricketAnalyser = new CricketAnalyser();
-        cricketAnalyser.getCricketDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
-        String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.AVGSTRIKE);
-        MostRunsCSV[] mostRunCsv = new Gson().fromJson(sortedCricketData, MostRunsCSV[].class);
+        CricketDTO[] mostRunCsv = new Gson().fromJson(sortedCricketData, CricketDTO[].class);
         Assert.assertEquals("MS Dhoni",mostRunCsv[0].player);
     }
 
+    //TC-2
+    @Test
+    public void givenIPLMostRunsCSVFile_ReturnsTopStrikingRate_InIPL2019() {
+        CricketAnalyser cricketAnalyser = new CricketAnalyser();
+        cricketAnalyser.getBattingDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
+        String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.STRIKERATE);
+        CricketDTO[] mostRunCsv = new Gson().fromJson(sortedCricketData, CricketDTO[].class);
+        Assert.assertEquals("Ishant Sharma",mostRunCsv[0].player);
+    }
+
+    //TC-3
+    @Test
+    public void givenIPLMostRunsCSVFile_ReturnsCricketer_WhoHitMaximum4sAnd6s_InIPL2019() {
+        CricketAnalyser cricketAnalyser = new CricketAnalyser();
+        cricketAnalyser.getBattingDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
+        String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.BESTSTRIKE);
+        CricketDTO[] mostRunCsv = new Gson().fromJson(sortedCricketData, CricketDTO[].class);
+        Assert.assertEquals("Andre Russell",mostRunCsv[0].player);
+    }
+
+    //TC-4
+    @Test
+    public void givenIPLMostRunsCSVFile_ReturnsCricketer_WhoHasMaximumStrikeRateWithMaximum4sAnd6s() {
+        CricketAnalyser cricketAnalyser = new CricketAnalyser();
+        cricketAnalyser.getBattingDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
+        String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.BESTSTRIKE);
+        CricketDTO[] mostRunCsv = new Gson().fromJson(sortedCricketData, CricketDTO[].class);
+        Assert.assertEquals("Andre Russell",mostRunCsv[0].player);
+    }
+
+    //TC-5
+    @Test
+    public void givenIPLMostRunsCSVFile_When_MaximumAverage_Returns_BestStrikeRate() {
+        CricketAnalyser cricketAnalyser = new CricketAnalyser();
+        cricketAnalyser.getBattingDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
+        String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.AVGSTRIKE);
+        CricketDTO[] mostRunCsv = new Gson().fromJson(sortedCricketData, CricketDTO[].class);
+        Assert.assertEquals("MS Dhoni",mostRunCsv[0].player);
+    }
+
+    //TC-6
     @Test
     public void givenIPLMostRunsCSVFile_WhenSortedOnAverage_ReturnsMaximumRuns() {
         CricketAnalyser cricketAnalyser = new CricketAnalyser();
-        cricketAnalyser.getCricketDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
+        cricketAnalyser.getBattingDataFile(IPL_2019_MOST_RUNS_FILE_PATH);
         String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.MAXRUNS);
-        MostRunsCSV[] mostRunCsv = new Gson().fromJson(sortedCricketData, MostRunsCSV[].class);
+        CricketDTO[] mostRunCsv = new Gson().fromJson(sortedCricketData, CricketDTO[].class);
         Assert.assertEquals("David Warner", mostRunCsv[0].player);
+    }
+
+    //TC-7
+    @Test
+    public void givenIPLMostWicketsCSVFile_ReturnsTopBowlingAverage_InIPL2019() {
+        CricketAnalyser cricketAnalyser = new CricketAnalyser();
+        cricketAnalyser.getBowlingDataFile(IPL_2019_MOST_WICKETS_FILE_PATH);
+        String sortedCricketData = cricketAnalyser.getSortedCricketData(SortedField.AVERAGE);
+        CricketDTO[] mostWicketsCsv = new Gson().fromJson(sortedCricketData, CricketDTO[].class);
+        Assert.assertEquals("Krishnappa Gowtham",mostWicketsCsv[0].player);
     }
 }
